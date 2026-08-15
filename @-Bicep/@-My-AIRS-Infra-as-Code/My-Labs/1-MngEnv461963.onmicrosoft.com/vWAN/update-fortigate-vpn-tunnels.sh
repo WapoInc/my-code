b1f49acecbf99ded787eea8ff0fa439d7d508981
Vince Resente <vresente@me.com>
2026-08-15 17:29:51 +0200

    exported fortigate config

--- @-Bicep/@-My-AIRS-Infra-as-Code/My-Labs/1-MngEnv461963.onmicrosoft.com/vWAN/update-fortigate-vpn-tunnels.sh
+++ @-Bicep/@-My-AIRS-Infra-as-Code/My-Labs/1-MngEnv461963.onmicrosoft.com/vWAN/update-fortigate-vpn-tunnels.sh
@@ -1,8 +1,8 @@
 config vpn ipsec phase1-interface
     edit "MiaCasa-Fort-1"
-        set remote-gw 4.253.194.231
+        set remote-gw 4.253.125.184
     next
     edit "MiaCasa-Fort-2"
-        set remote-gw 4.253.24.172
+        set remote-gw 4.253.6.166
     next
 end
