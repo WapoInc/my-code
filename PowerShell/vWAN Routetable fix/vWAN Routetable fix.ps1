accfae43f8290fdb69c67dec8bdc4eb3c1b2b814
Vince Resente <vresente@me.com>
2026-09-03 08:32:40 +0200

    vWAN variables updated

--- PowerShell/vWAN Routetable fix/vWAN Routetable fix.ps1
+++ PowerShell/vWAN Routetable fix/vWAN Routetable fix.ps1
@@ -1,17 +1,18 @@
 ﻿
 Connect-AzAccount
 Get-AzSubscription
-Select-AzSubscription -SubscriptionName "@viresent - AIRS"
+Select-AzSubscription -SubscriptionName "viresent-New-AIRS"
 
 
 
 
-$rt = Get-AzVHubRouteTable -ResourceGroupName MTN-vWAN-Demo-rg -ParentResourceName ZAW_HUB -Name RouteTable-defaultRouteTable -debug -verbose
+$rt = Get-AzVHubRouteTable -ResourceGroupName Global-vWAN-rg -ParentResourceName ZAN-Hub-1 -Name RouteTable-defaultRouteTable -debug -verbose
 
-$rt = Get-AzVHubRouteTable -ResourceGroupName MTN-vWAN-Demo-rg -ParentResourceName ZAW_HUB -Name RouteTable-noneRouteTable -debug -verbose
+$rt = Get-AzVHubRouteTable -ResourceGroupName Global-vWAN-rg -ParentResourceName ZAW-Hub-1 -Name RouteTable-noneRouteTable -debug -verbose
 
 Update-AzVHubRouteTable -InputObject $rt -debug -verbose  -debug -verbose
 
 Get/Put on the vhub without any changes to get it out of failed state
  
-$hub = Get-AzVirtualHub -Name ZAW_HUB -ResourceGroupName MTN-vWAN-Demo-rg -debug -verbose
+$hub = Get-AzVirtualHub -Name ZAN-Hub-1 -ResourceGroupName MTN-vWAN-Demo-rg -debug -verbose
+$hub = Get-AzVirtualHub -Name ZAW-Hub-1 -ResourceGroupName MTN-vWAN-Demo-rg -debug -verbose
