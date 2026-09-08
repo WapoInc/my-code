4b9bf1d14cdd61ff92babdcf565258fdb31038fc
Vince Resente <vresente@me.com>
2026-09-08 05:58:35 +0200

    xx

--- Bicep/AIRS-Infra-as-Code/My-Labs/1-MngEnv461963.onmicrosoft.com/vWAN/update-fortigate-vpn-tunnels.sh
+++ Bicep/AIRS-Infra-as-Code/My-Labs/1-MngEnv461963.onmicrosoft.com/vWAN/update-fortigate-vpn-tunnels.sh
@@ -1,8 +1,8 @@
 config vpn ipsec phase1-interface
     edit "MiaCasa-Fort-1"
-        set remote-gw 102.133.130.236
+        set remote-gw 4.253.18.143
     next
     edit "MiaCasa-Fort-2"
-        set remote-gw 4.253.3.227
+        set remote-gw 4.253.18.145
     next
 end
